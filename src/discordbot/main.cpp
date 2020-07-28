3a7d87984b955860471050272aa660718969742a
Oussama Khalaf <o.khalaf@digipen.edu>
2020-07-28 00:28:30 -0700

    the discord "shell" is done
    takes a list of server members and makes it into a vector of contestants

--- src/discordbot/main.cpp
+++ src/discordbot/main.cpp
@@ -14,10 +14,35 @@ class MyClientClass : public SleepyDiscord::DiscordClient
 {
 public:
 	using SleepyDiscord::DiscordClient::DiscordClient;
+
 	void onMessage(SleepyDiscord::Message message) override
 	{
-		if (message.startsWith("whcg hello"))
-			sendMessage(message.channelID, "Hello " + message.author.username);
+		if (message.startsWith("Pog"))
+		{
+			//sends a message to the same channel that Pog was received in and then turns it into a link to their avatar using their ID and avatar hash and then add a .png link
+			
+			SleepyDiscord::Server guild = getServer(message.serverID);
+			auto members = listMembers(message.serverID,12).list();
+			
+			std::vector<Contestant> test_contestants;
+
+			for (SleepyDiscord::ServerMember& member : members) {
+				std::string pfp = "https:\//cdn.discordapp.com/avatars/" + member.user.ID + "/" + member.user.avatar + ".png";
+				test_contestants.emplace_back(Contestant{ pfp, member.user.username,0,true });
+			}
+			sendTyping(message.channelID);
+			auto events = simulate(test_contestants);
+			std::string eventsList;
+			for (Event& event : events)
+			{
+				for (std::string& chunk : event.text)
+				{
+					eventsList += chunk;
+				}
+				eventsList += "\n";
+			}
+			sendMessage(message.channelID, eventsList);
+		}
 	}
 };
 
